2f1794e804494f5d4154ab8344362a0941469890
Curtis Ullerich <curtisullerich@gmail.com>
2013-01-07 12:04:52 -0600

    finished dict implementation

--- markov.py
+++ markov.py
@@ -8,27 +8,44 @@ load("STRUM2")
 
 """
 from Chapter 19 (Markov Processes) of Notes from the Metalevel
-
-I chose to represent transition tables as matrices (2D arrays)
-where the entry at table[2][3] is the probability of transition
-from state 2 to state 3.
 """
 
-#index values for each row/column in the table.
-q = 0 #quarter note
-e = 1 #eighth note
-ed = 2 #dotted eighth
-s = 3 #sixteenth
-w = 4 #wait (stop and return)
-rhydict = dict(q=dict(q=.5,e=2,ed=.75),e=dict(e=3,q=1),ed=dict(s=1),s=dict(e=2,q=1))
-rhydict2 = dict(q=dict(q=.5,e=2,ed=.75,w=.2),e=dict(e=3,q=1),ed=dict(s=1),s=dict(e=2,q=1),w=dict(stop=1))
-intmix = [[0,0,.4,.4,0.1],[0,.2,.4,.4,.1],[.2,.6,0,.4,0],[0,.2,.4,.4,0],[0,.4,.2,.2,0]]
+#both are valid dictionary instantiations
+rhydict  = dict(
+q=dict(q=.5,e=2,ed=.75),
+e=dict(e=3,q=1),
+ed=dict(s=1),s=dict(e=2,q=1)
+)
+rhydict2 = dict(
+q=dict(q=.5,e=2,ed=.75,w=.2),
+e=dict(e=3,q=1),ed=dict(s=1),
+s=dict(e=2,q=1),
+w=dict(stop=1)
+)
+rhydict3 = dict(
+q=dict(q=.5,e=2,ed=1.5,w=.2),
+e=dict(e=2.5,q=1),
+ed=dict(s=1,ed=.3),
+s=dict(e=2,q=1,s=3),
+w=dict(stop=1)
+)
+intmix   = {
+1:{3:.4,4:.4,6:.1},
+2:{2:.2,3:.4,4:.4,6:.1},
+3:{1:.2,2:.6,4:.4},
+4:{2:.2,3:.4,4:.4},
+6:{2:.4,3:.2,4:.2}
+}
+intmix2  = {
+4: {4:.2,7:.5,12:.3},
+7: {4:.1,7:.1,12:.1},
+12:{4:.2,7:.3,12:.4}
+}
 
 #Uses markov chains to generate rhythms for a drunk sequence of notes
 #Use rhytable  for the first exercise from Metalevel
 #and rhytable2 for the second exercise. This function works for both.
-def markovRhythms(length, quarterNote):
-  tbl = rhydict2
+def markovRhythms(length, quarterNote, tbl):
   r =  'q'
   drunk = Drunk(60, 6, 40, 80, 3, 10)
   timepoint = 0
@@ -54,55 +71,30 @@ def markovRhythms(length, quarterNote):
     STRUM2(timepoint, pulse, amp, keynumToHertz(k), 1, 1.0, 0.5)
     timepoint += dur
 
-def markovChorder(length, intmix, note, size, ud, rhy, dur):
-  intt = 1
+def markovChorder(length, intmix, note, size, ud, rhy, dur, begin):
+  intt = begin
   key = note
   chord = False
   timepoint = 0
   for i in range(length):
-    intt = selectNext(intmix, intt)
+    intt = selectNextFromDict(intmix, intt)
     if (random.random() < ud):
-      key += indexToChordInt(intt)
+      key += intt
       if (key > 90):
         key = 90
     else:
-      key -= indexToChordInt(intt)
+      key -= intt
       if (key < 50):
         key = 50
     chord = []
     for i in range(size):
       n = key
-      intt = selectNext(intmix, intt)
-      n = n - indexToChordInt(intt)
+      intt = selectNextFromDict(intmix, intt)
+      n = n - intt
       chord.append(n)    
     for c in chord:
       STRUM2(timepoint, dur, 10000, keynumToHertz(c), 1, 1.0, 0.5)    
     timepoint += rhy
-
-
-def indexToChordInt(intt):
-  if (intt <= 3):
-    return intt + 1
-  else:
-    return intt + 2
-
-#select the next element of the markov transition table
-#given the past value
-def selectNext(table, past):
-  r = random.uniform(0, sum(table[past]))
-  #print "row: " + str(table[past]) 
-  #print "sum: " + str(sum(table[past]))
-  #print "past: " + str(past)
-  #print "r: " + str(r)
-  rsum = 0
-  for i in range(len(table[past])):
-    rsum += table[past][i]
-    #print "curval=" + str(table[past][i])
-    #print "is %f < %f?" % (r, rsum)
-    if (r < rsum):
-      return i
-  #give up and return the last one
-  return len(table[past]) - 1
 
 #select the next element of the markov transition table
 #dictionary implementation given the past value
@@ -127,8 +119,10 @@ def selectNextFromDict(table, past):
   #give up and return the last one
   return table.items().pop()
   
-  
-random.seed(3)
-markovRhythms(100, .4)
-#markovChorder(25, intmix, 60, 6, .6, 1.2, 1.2)
+random.seed(1)
+#markovRhythms(100, .4, rhydict)
+#markovRhythms(100, .4, rhydict2)
+#markovRhythms(100, .4, rhydict3)
+#markovChorder(25, intmix,  60, 6, .6, 1.2, 1.2, 1)
+markovChorder(25, intmix2, 60, 6, .6, .6, 1, 4)
 
