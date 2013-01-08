46739ebb7555810e918f7a0e3ba9c91ca5932102
Curtis Ullerich <curtisullerich@gmail.com>
2013-01-08 13:41:46 -0600

    adding map functions

--- metalevel/jazz.py
+++ metalevel/jazz.py
@@ -14,3 +14,4 @@ load("STRUM2")
 jazzscale = [0, 2, 3, 5, 7, 9, 10, 12, 14] # dorian with decorated octave
 jazzchanges = ['bf3', 'ef4', 'bf3', 'bf', 'ef4', 'ef', 'bf3', 'bf', 'f4', 'ef', 'bf3', 'bf']
 jazztempo = 120
+
